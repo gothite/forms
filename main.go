15e4ebda6fc4360929f18b241ac812a422a79e33
Vitalii Maslov <hello@bindlock.me>
2019-01-08 14:11:02 +0300

    Updated base form.

--- main.go
+++ main.go
@@ -3,29 +3,38 @@ package forms
 import (
 	"encoding/json"
 	"io"
+	"mime/multipart"
 	"net/url"
 )
 
 type Clean func(data Data) error
+type Schema []Clean
 
 type Form interface {
-	Schema() []Clean
-	GetError(field string, code uint, parameters ...interface{}) error
+	Schema() Schema
+	Messages() map[uint]string
+	FieldError(field string, code uint, parameters ...interface{}) error
+	FormError(code uint, errors ...error) error
 	Clean(data Data) error
 	OnError(err *FormError) error
 }
 
 type BaseForm struct{}
 
-func (form *BaseForm) GetError(field string, code uint, parameters ...interface{}) error {
-	var message string
-	var ok bool
+func (form *BaseForm) Messages() map[uint]string {
+	return Messages
+}
 
-	if message, ok = Errors[code]; !ok {
-		message = Errors[Unknown]
-	}
+func (form *BaseForm) FieldError(field string, code uint, parameters ...interface{}) error {
+	return NewFieldError(code, field, Messages[Unknown], parameters)
+}
 
-	return NewFieldError(code, field, message, parameters)
+func (form *BaseForm) FormError(code uint, errors ...error) error {
+	return NewFormError(code, Messages[Invalid], errors...)
+}
+
+func (form *BaseForm) Schema() Schema {
+	return nil
 }
 
 func (form *BaseForm) Clean(_ Data) error {
@@ -40,7 +49,7 @@ func ValidateJSON(form Form, reader io.Reader) error {
 	var data map[string]interface{}
 
 	if err := json.NewDecoder(reader).Decode(&data); err != nil {
-		return form.OnError(NewFormError(InvalidJSON, Errors[InvalidJSON]))
+		return form.OnError(NewFormError(InvalidJSON, Messages[InvalidJSON]))
 	}
 
 	return Validate(form, JSON(data))
@@ -50,18 +59,47 @@ func ValidateForm(form Form, data url.Values) error {
 	return Validate(form, FormData(data))
 }
 
+func ValidateMultipartForm(form Form, data *multipart.Form) error {
+	return Validate(form, MultipartFormData{data})
+}
+
+func getMessage(code uint, empty string, messages ...map[uint]string) string {
+	for _, messages := range messages {
+		if message, ok := messages[code]; ok {
+			return message
+		}
+	}
+
+	return empty
+}
+
 func Validate(form Form, data Data) error {
 	var errors []error
+	var messages = form.Messages()
 
 	for _, clean := range form.Schema() {
 		if err := clean(data); err != nil {
+			if err, ok := err.(*FieldError); ok {
+				err.Message = getMessage(err.Code, err.Message, messages, Messages)
+			}
+
 			errors = append(errors, err)
 		}
 	}
 
 	if errors != nil {
-		return form.OnError(NewFormError(Invalid, Errors[InvalidJSON], errors...))
+		return form.OnError(NewFormError(Invalid, getMessage(Invalid, Messages[Unknown], messages, Messages), errors...))
 	}
 
-	return form.Clean(data)
+	err := form.Clean(data)
+
+	if err, ok := err.(*FormError); ok {
+		err.Message = getMessage(err.Code, err.Message, messages, Messages)
+	}
+
+	if err, ok := err.(*FieldError); ok {
+		err.Message = getMessage(err.Code, err.Message, messages, Messages)
+	}
+
+	return err
 }
--- main_test.go
+++ main_test.go
@@ -2,31 +2,35 @@ package forms
 
 import (
 	"bytes"
+	"mime/multipart"
 	"net/url"
 	"testing"
 )
 
-var InvalidCredentials = NewFormError(101, "Incorrect username or password!", nil)
+var IncorrectCredentials uint = 101
+var messages = map[uint]string{IncorrectCredentials: "Incorrect username or password!"}
 
 type LoginForm struct {
 	BaseForm
 
 	Username string
 	Password string
-
-	schema []Clean
 }
 
-func (form *LoginForm) Schema() []Clean {
-	return []Clean{
+func (form *LoginForm) Schema() Schema {
+	return Schema{
 		form.CleanUsername,
 		form.CleanPassword,
 	}
 }
 
+func (form *LoginForm) Messages() map[uint]string {
+	return messages
+}
+
 func (form *LoginForm) Clean(_ Data) error {
 	if form.Username != form.Password {
-		return InvalidCredentials
+		return form.FormError(101)
 	}
 
 	return nil
@@ -34,9 +38,9 @@ func (form *LoginForm) Clean(_ Data) error {
 
 func (form *LoginForm) CleanUsername(data Data) error {
 	if value := data.Get("username"); value == nil {
-		return form.GetError("username", Required)
+		return form.FieldError("username", IncorrectCredentials)
 	} else if value, ok := value.(string); !ok {
-		return form.GetError("username", Invalid)
+		return form.FieldError("username", Invalid)
 	} else {
 		form.Username = value
 	}
@@ -46,9 +50,9 @@ func (form *LoginForm) CleanUsername(data Data) error {
 
 func (form *LoginForm) CleanPassword(data Data) error {
 	if value := data.Get("password"); value == nil {
-		return form.GetError("password", Required)
+		return form.FieldError("password", Unknown)
 	} else if value, ok := value.(string); !ok {
-		return form.GetError("password", Invalid)
+		return form.FieldError("password", Invalid)
 	} else {
 		form.Password = value
 	}
@@ -58,7 +62,17 @@ func (form *LoginForm) CleanPassword(data Data) error {
 
 func TestValidateJSON(test *testing.T) {
 	var form = &LoginForm{}
-	var data = bytes.NewBufferString(`{"username": "bindlock", "password": "bindlock"}`)
+	var data = bytes.NewBufferString(`{"username": "bindlock",`)
+
+	if err := ValidateJSON(form, data); err == nil {
+		test.Fatal("Expected error!")
+	} else if err := err.(*FormError); err.Code != InvalidJSON {
+		test.Errorf("Incorrect code!")
+		test.Errorf("Expected: %d", InvalidJSON)
+		test.Fatalf("Actual: %d", err.Code)
+	}
+
+	data = bytes.NewBufferString(`{"username": "bindlock", "password": "bindlock"}`)
 
 	if err := ValidateJSON(form, data); err != nil {
 		test.Fatal(err)
@@ -67,13 +81,56 @@ func TestValidateJSON(test *testing.T) {
 		test.Errorf("Expected: bindlock")
 		test.Fatalf("Actual: %s", form.Username)
 	}
+
+	data = bytes.NewBufferString(`{"username": "bindlock"}`)
+
+	if err := ValidateJSON(form, data); err == nil {
+		test.Fatal("Expected error!")
+	} else if err := err.(*FormError); err.Code != Invalid {
+		test.Errorf("Incorrect code!")
+		test.Errorf("Expected: %d", Invalid)
+		test.Fatalf("Actual: %d", err.Code)
+	}
 }
 
 func TestValidateForm(test *testing.T) {
 	var form = &LoginForm{}
-	var data = url.Values{"username": []string{"bindlock"}, "password": []string{"bindlock"}}
+	var data = url.Values{"username": {"bindlock"}, "password": {"bindlock"}}
 
 	if err := ValidateForm(form, data); err != nil {
+		test.Fatal(err)
+	} else if form.Username != "bindlock" || form.Username != form.Password {
+		test.Errorf("Incorrect values!")
+		test.Errorf("Expected: bindlock")
+		test.Fatalf("Actual: %s", form.Username)
+	}
+
+	data = url.Values{"username": {"bindlock"}, "password": {"lock"}}
+
+	if err := ValidateForm(form, data); err == nil {
+		test.Fatal("Expected error!")
+	} else if message := err.(*FormError).Message; message != messages[101] {
+		test.Errorf("Incorrect error message!")
+		test.Errorf("Expected: %s", messages[101])
+		test.Fatalf("Actual: %s", message)
+	}
+
+	data = url.Values{}
+
+	if err := ValidateForm(form, data); err == nil {
+		test.Fatal("Expected error!")
+	} else if message := err.(*FormError).Message; message != Messages[Invalid] {
+		test.Errorf("Incorrect error message!")
+		test.Errorf("Expected: %s", Messages[Invalid])
+		test.Fatalf("Actual: %s", message)
+	}
+}
+
+func TestValidateMultipartForm(test *testing.T) {
+	var form = &LoginForm{}
+	var data = &multipart.Form{Value: map[string][]string{"username": {"bindlock"}, "password": {"bindlock"}}}
+
+	if err := ValidateMultipartForm(form, data); err != nil {
 		test.Fatal(err)
 	} else if form.Username != "bindlock" || form.Username != form.Password {
 		test.Errorf("Incorrect values!")
